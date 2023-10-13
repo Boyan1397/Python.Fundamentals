6c5eaaf5540e4726869fbb0428afe710b3321148
Boyan1397 <144154206+Boyan1397@users.noreply.github.com>
2023-10-13 21:09:26 +0300

    updated_file

---
+++ functions_exercise/test.py
@@ -0,0 +1,7 @@
+def sum_numbers(first_num, second_num):
+    return first_num + second_num
+
+first_number = int(input())
+second_number = int(input())
+third_number = int(input())
+print(sum_numbers(first_number, second_number))
